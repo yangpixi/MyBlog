1e3eae5d85c150437d561692244795bb8fba7448
yangpixi <yang20070135@gmail.com>
2025-09-04 12:29:36 +0800

    完成后台登录界面

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: () => import('@/views/WelcomPage.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue')
+    }
+  ],
 })
 
 export default router
