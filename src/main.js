41ca7defb4ee1281de9ca76fc03a951275652893
yangpixi <2602340701@qq.com>
2025-09-05 11:20:49 +0800

    完成登录界面登录机制

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8080'
 
 const app = createApp(App)
 
