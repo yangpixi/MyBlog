b15c435be55e1ee3b5930d8b2408a310fc94ef82
yangpixi <yang20070135@gmail.com>
2025-09-05 16:31:49 +0800

    完成导航栏及主页背景

--- src/views/WelcomPage.vue
+++ src/views/WelcomPage.vue
@@ -1,24 +1,49 @@
 <script>
-import router from '@/router';
+import Navigation from '@/components/navigation.vue';
 
-export default {
-    data() {
-        return {
 
-        }
-    },
-    methods: {
-        toLogin() {
-            router.push('/login');
-        },
-    }
-}
 </script>
 
 <template>
-    <h1>主界面</h1>
-    <el-button round @click="toLogin" type="primary">去登录</el-button>
+    <Navigation></Navigation>
+
+    <div class="background">
+        <div class="content">
+
+
+        </div>
+    </div>
+
 </template>
 
 <style scoped>
+* { 
+    font-size: 10px;
+    box-sizing: border-box;
+    margin: 0;
+}
+
+.background {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+}
+
+.background::before {
+    content: '';
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    width: calc(100% + 10px);
+    height: calc(100% + 10px);
+    background-image: url('/src/assest/back.jpg');
+    background-size: cover;
+    background-position: center;
+    background-color: rgba(0, 0, 0, 0.3);
+    filter: blur(5px);
+    z-index: 1;
+    /* background-attachment: fixed; */
+}
+
 </style>
